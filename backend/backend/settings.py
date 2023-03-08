5926c9b16975ff62ee08f207f210b49b02256c82
Ming Han <101275733+lawminghan@users.noreply.github.com>
2023-03-08 18:45:46 +0800

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authenticationAPI',
+    'backend.authenticationAPI',
     'rest_framework',
     'rest_framework_simplejwt',
 ]
