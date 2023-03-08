b46352beb5e857c2d243e51b922320a8953da235
lawminghan <lawminghan2000@gmail.com>
2023-03-08 23:49:46 +0800

    refactor: moved files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-+_j83m24rk7hzw@wh+zeea)_pqyjoqj(ooll_k7%z*1bt07jik')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
