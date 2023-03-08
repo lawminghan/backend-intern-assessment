7d3546999dd0a96ee2d96299755822b8cb5e1151
lawminghan <lawminghan2000@gmail.com>
2023-03-08 23:10:22 +0800

    fix: Render setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backend.authenticationAPI',
+    'authenticationAPI',
     'rest_framework',
     'rest_framework_simplejwt',
 ]
